cebaf24d7655f578159ca58817538c820cdcc9a0
Becca Stanger <beccastanger@gmail.com>
2018-01-31 11:04:12 -0800

    Complete React Components

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// TODO
+// var Milk = () => (
+//   <li>Milk</li>
+// );
+//
+// var Eggs = () => (
+//   <li>Eggs</li>
+// );
+
+// var GroceryList = () => (
+//   <ul>
+//     <GroceryListItem groceryItems={['Milk']} />
+//     <GroceryListItem groceryItems={['Eggs']} />
+//   </ul>
+// );
+
+// var GroceryListItem = (props) => (
+//   <li>{props.groceryItems}</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      hovered: false
+    }
+  }
+
+  onGroceryListItemHover (){
+    this.setState({
+      hovered: !this.state.hovered
+    })
+  }
+
+  render(){
+    var style = {
+      fontWeight: this.state.hovered ? 'bold' : 'normal'
+    }
+
+    return (
+      <li style={style} onMouseOver={this.onGroceryListItemHover.bind(this)}>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(groceryItem =>
+      <GroceryListItem groceryItem={groceryItem} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList groceryItems={["Milk", "Eggs"]}/>, document.getElementById("app"));
